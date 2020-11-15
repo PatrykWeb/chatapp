831428f08a7732d712a930d5719cb5d9ccbab661
devWiseguy <hillj1089@gmail.com>
2020-11-15 21:20:33 +0100

    added showing messages from the database on the home page

--- src/App.js
+++ src/App.js
@@ -26,7 +26,9 @@ function App() {
       <header>
           <section>
             {user ? <ChatRoom/> : <SignIn />}
+              <SignOut />
           </section>
+
       </header>
     </div>
   );
@@ -34,19 +36,33 @@ function App() {
   function SignIn() {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider()
-    auth.signInWithPopup(provider).then(r => console.log(r)).catch(console.log);
+    auth.signInWithPopup(provider).then(r => console.log("Zalogowanie przeszło pomyślnie")).catch(console.log);
   }
     return (
       <button onClick={signInWithGoogle}>Zaloguj sie za pomocą google</button>
     )
   }
 
+  function SignOut() {
+    return auth.currentUser && (
+        <button onClick={() => auth.signOut()}>Wyloguj sie</button>
+    );
+  }
+
   function ChatRoom() {
+    const messagesChat = firestore.collection("messages");
+    const query = messagesChat.orderBy("createdAt").limit(25);
+    const [messagesUsers] = useCollectionData(query, {idField: 'id'})
     return(
         <div>
-
+            {messagesUsers && messagesUsers.map(msg => <ChatMessage key = {msg.id} message = {msg}/>)}
         </div>
     )
+  }
+  function ChatMessage(props) {
+    console.log("4:00")
+      const { text, uid } = props.message;
+      return <p>{text}</p>
   }
 
 
